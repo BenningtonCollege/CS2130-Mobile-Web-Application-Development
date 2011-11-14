72f09931a13b12d79615ce96ce3dd4f351cf59c3
William Doane <wil@DrDoane.com>
2011-11-13 21:22:59 -0500

    Improve organization of code so that events only happen when specific pages are viewed

--- localStorage-external-JS/custom.js
+++ localStorage-external-JS/custom.js
@@ -2,16 +2,22 @@ $(document).bind("mobileinit", function() {
   $.mobile.page.prototype.options.addBackBtn = true;
 });
 
-$(document).bind("pagebeforeshow", function(event, ui) {
-  // Using localStorage to control the text or images displayed on pages.
+$('#home').live('pageinit', function(event, ui) {
   $("#useroption")[0].innerHTML = localStorage.firstname + " " + localStorage.lastname;
+});
+
+$('#userProfile').live('pageinit', function(event, ui) {
+  // Using localStorage to control the text or images displayed on pages.
   $("#profilePicture")[0].src = "images/" + localStorage.lastname.toLowerCase() + "-" + localStorage.firstname.toLowerCase() + ".jpg";
   //           images/doane-william.jpg
+});
 
+$('#settings').live('pageinit', function(event, ui) {
   // Setting the value of form fields
   $("#firstname").val(localStorage.firstname);
   $("#lastname").val(localStorage.lastname);
 });
+
 
 function submitForm() {
   localStorage.firstname = $("#firstname").val();
